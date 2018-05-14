dfa8887726b73f563a7e273e962fab675a9803bb
vlapouble <vianney.lapouble@gmail.com>
2018-05-14 17:01:34 +0200

    Update example

--- spark-kafka/example/project/src/main/scala/spark/KafkaExample.scala
+++ spark-kafka/example/project/src/main/scala/spark/KafkaExample.scala
@@ -1,23 +1,24 @@
 package org.hands.on.spark
 
-import org.apache.spark.SparkContext
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.functions._
 import org.apache.spark.sql._
 import org.apache.spark.sql.types._
+import org.spark.implicits._
 
 /** Main class */
 object KafkaExample {
 
-  import org.apache.spark.sql.SparkSession
-  import org.apache.spark.SparkConf
-  import org.apache.spark.sql.functions._
 
-  import org.spark.implicits
+
+
 
   val spark : SparkSession =
     SparkSession
       .builder()
       .appName("")
-      .config("spark.master", "local[*]")
+      .config("spark.master", "spark://master:7077")
       .getOrCreate()
 
   /** Main function */
